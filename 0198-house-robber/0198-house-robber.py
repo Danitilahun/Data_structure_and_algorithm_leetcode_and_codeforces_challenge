e01831bae0f83d5e81a8ab565a6f07f37bf45da4
Daniel Tilahun <106193338+Danitilahun@users.noreply.github.com>
2025-07-05 21:39:21 +0300

    Time: 0 ms (100%), Space: 18 MB (13.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -12,18 +12,36 @@ class Solution:
         # return helper(len(nums)-1)
 
         # DP Memoization 
-        dp = [-1] * len(nums)
-        def helper(idx):
-            if idx == 0: return nums[idx]
-            if idx < 0 : return 0
+        
+        # dp = [-1] * len(nums)
+        # def helper(idx):
+        #     if idx == 0: return nums[idx]
+        #     if idx < 0 : return 0
 
-            if dp[idx] != -1: return dp[idx]
+        #     if dp[idx] != -1: return dp[idx]
 
-            left = nums[idx] + helper(idx - 2)
-            right = 0 + helper(idx -1)
+        #     left = nums[idx] + helper(idx - 2)
+        #     right = 0 + helper(idx -1)
 
-            dp[idx] = max(left, right)
+        #     dp[idx] = max(left, right)
 
-            return dp[idx]
+        #     return dp[idx]
+
+        # return helper(len(nums)-1)
+
+        # DP tabulation 
+        length = len(nums)
+        dp = [-1] * length
+        dp[0] = nums[0]
+        answer = dp[0] 
+
+        for idx in range(1,length):
+            take = nums[idx] + (dp[idx-2] if idx - 2 >= 0 else 0)
+            not_take = 0 + dp[idx-1]
             
-        return helper(len(nums)-1)
+            dp[idx] = max(take, not_take)
+            
+            answer = max(answer, dp[idx])
+        return answer
+
+
