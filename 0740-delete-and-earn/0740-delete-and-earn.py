e579b3cc83bae04d089bbaae468b014c55a7b6d1
Daniel Tilahun <106193338+Danitilahun@users.noreply.github.com>
2025-07-11 21:16:49 +0300

    Time: 110 ms (5.03%), Space: 19 MB (98.6%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,21 +1,42 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         freq = [0]*(10002)
-        dp = [-1]*(10002)
+
         for num in nums:
             freq[num]+=1
+
+        dp = [-1]*(10002)
+
+        # DP - Memoization
     
-        def helper(idx):
+        # def helper(idx):
 
-            if idx <= 0 : return 0
-            if dp[idx] != -1: return dp[idx]
+        #     if idx <= 0 : return 0
+        #     if dp[idx] != -1: return dp[idx]
 
-            take = idx * freq[idx] + helper(idx - 2)
-            not_take = 0 + helper(idx - 1)
+        #     take = idx * freq[idx] + helper(idx - 2)
+        #     not_take = 0 + helper(idx - 1)
 
-            dp[idx] = max(take,not_take)
+        #     dp[idx] = max(take,not_take)
 
-            return dp[idx]
+        #     return dp[idx]
         
-        return helper(10001)
+        # return helper(10001)
+
+        # DP - Tabulation
+
+        dp[0] = 0
+        dp[1] = freq[1]
+        answer = dp[1]
+        for idx in range(2,10002):
+
+            take = idx * freq[idx] + dp[idx-2]
+            not_take = 0 + dp[idx-1]
+
+            dp[idx] = max(take, not_take)
+
+            answer = max(answer,dp[idx])
+        
+        return answer
+            
         
