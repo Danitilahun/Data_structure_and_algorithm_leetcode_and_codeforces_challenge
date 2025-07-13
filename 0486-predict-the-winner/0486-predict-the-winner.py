b20c2eba2e36b9b8be9aa0429f0c7bb152a5b14e
Daniel Tilahun <106193338+Danitilahun@users.noreply.github.com>
2025-07-13 23:36:41 +0300

    Time: 2824 ms (11.9%), Space: 17.9 MB (61.9%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,16 +1,16 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
         
-        def helper(left: int, right: int, turn: bool):
+        def helper(left: int, right: int, turn: int):
             
-            if left >= right:
+            if left == right:
                 return nums[left]
             
-            take_from_left = (nums[left] if turn else -nums[left]) + helper(left+1,right, not turn)
-            take_from_right = (nums[right] if turn else -nums[right]) + helper(left,right-1, not turn)
+            take_from_left = nums[left]*turn + helper(left+1,right, -turn)
+            take_from_right = nums[right]*turn + helper(left,right-1, -turn)
 
-            return max(take_from_left, take_from_right) if turn else min(take_from_left, take_from_right)
+            return max(take_from_left, take_from_right) if turn==1 else min(take_from_left, take_from_right)
         
-        game = helper(0,len(nums)-1, True)
+        game = helper(0,len(nums)-1, 1)
 
         return True if game >= 0 else False
