b91e9a04d500433b096c21b2d846d9a03a7e379e
Daniel Tilahun <106193338+Danitilahun@users.noreply.github.com>
2025-07-14 23:15:29 +0300

    Time: 0 ms (100%), Space: 17.7 MB (48.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,20 +1,36 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        dp=[-1]*n
-        def count_distinct_ways(step_sum):
-            if step_sum > n:
-                return 0
-            if step_sum == n:
-                return 1
-            if dp[step_sum] != -1:
-                return dp[step_sum]
+        # dp=[-1]*n
+
+        # def count_distinct_ways(step_sum):
+        #     if step_sum > n:
+        #         return 0
+        #     if step_sum == n:
+        #         return 1
+        #     if dp[step_sum] != -1:
+        #         return dp[step_sum]
             
-            one_step_move = count_distinct_ways(step_sum+1)
-            two_step_move = count_distinct_ways(step_sum+2)
+        #     one_step_move = count_distinct_ways(step_sum+1)
+        #     two_step_move = count_distinct_ways(step_sum+2)
 
-            dp[step_sum] = one_step_move + two_step_move
+        #     dp[step_sum] = one_step_move + two_step_move
 
-            return dp[step_sum]
+        #     return dp[step_sum]
         
-        return count_distinct_ways(0)
+        # return count_distinct_ways(0)
+
+        if (n<=3):
+            return n
+
+        dp=[-1]*n
+
+        dp[0] = 1
+        dp[1] = 2
+
+        for idx in range(2,n):
+
+            dp[idx] = dp[idx-1] + dp[idx-2]
+        
+        return dp[n-1]
+            
