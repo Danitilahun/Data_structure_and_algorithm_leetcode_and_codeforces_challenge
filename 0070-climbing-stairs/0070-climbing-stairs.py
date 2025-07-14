571d3aafe2dbd4df20d4d609a7aea626f1631c94
Daniel Tilahun <106193338+Danitilahun@users.noreply.github.com>
2025-07-14 23:17:57 +0300

    Time: 0 ms (100%), Space: 17.6 MB (78.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+
+        if (n<=3):
+            return n
         
         # dp=[-1]*n
 
@@ -20,17 +23,25 @@ class Solution:
         
         # return count_distinct_ways(0)
 
-        if (n<=3):
-            return n
+        # dp=[-1]*n
 
-        dp=[-1]*n
+        # dp[0] = 1
+        # dp[1] = 2
 
-        dp[0] = 1
-        dp[1] = 2
+        # for idx in range(2,n):
+
+        #     dp[idx] = dp[idx-1] + dp[idx-2]
+        
+        # return dp[n-1]
+
+        previous_previous = 1
+        previous = 2
 
         for idx in range(2,n):
 
-            dp[idx] = dp[idx-1] + dp[idx-2]
+            current = previous + previous_previous
+            previous_previous = previous
+            previous = current
         
-        return dp[n-1]
+        return previous
             
