28563555801fa316d9a5ea10df41de234dbdfdd6
Daniel Tilahun <106193338+Danitilahun@users.noreply.github.com>
2026-01-01 16:14:34 +0300

    Time: 48 ms (53.34%), Space: 19.3 MB (95.94%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        """
+        Approach 1: Using set 
+        - Iterating over the linkedlist, check if node already exist in the set or not, 
+        if exist return that True, else put the node in the set.
+        - Where N is the number of nodes till the first Cyclic node.
+        Time Complexity : O(N)
+        Space Complexity : O(N)
+
+        Approach 2: Floyd's Cycle Detection Algorithm, also known as the "Tortoise and Hare" algorithm
+        - Create two variables(pointers) named tortoise and hare. Assign head for both.
+        - hare moves twice as fast as tortoise. Since there is a cycle after some time they will meet 
+        if there is cycle. 
+
+
+        Edge cases:
+        0. When the head is None
+        1. When there is only one element
+
+
+        Time Complexity : O(N)
+        Space Complexity : O(1)
+        """
+
+        if not head or not head.next:
+            return False
+
+        hare = head
+        tortoise = head
+
+        while tortoise:
+            hare = hare.next
+            
+            tortoise = tortoise.next
+
+            if tortoise:
+                tortoise = tortoise.next
+            
+            if hare == tortoise:
+                return True
+        
+        return False
