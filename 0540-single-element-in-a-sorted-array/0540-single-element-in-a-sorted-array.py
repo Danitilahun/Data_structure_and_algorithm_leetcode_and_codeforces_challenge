4c417c9912a9c747985d9cc29078e49730246548
Daniel Tilahun <106193338+Danitilahun@users.noreply.github.com>
2026-01-18 23:04:58 +0300

    Time: 0 ms (100%), Space: 27 MB (26.2%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,12 +1,31 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        """
-        Approach 1: using XOR property 
-        """
-
-        unique = nums[0]
+    
         length = len(nums)
-        for index in range(1,length):
-            unique ^= nums[index]
         
-        return unique
+        if length == 1 or nums[0] != nums[1]:
+            return nums[0]
+        
+        if nums[length - 1] != nums[length - 2]:
+            return nums[length - 1]
+
+        low = 1
+        high = length - 2
+        while low <= high:
+            mid = (low + high)//2
+
+            if nums[mid-1] != nums[mid] and nums[mid+1] != nums[mid]:
+                return nums[mid]
+            
+            if mid%2 == 0: 
+                if nums[mid+1] == nums[mid]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            elif mid%2 != 0:
+                if nums[mid+1] == nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+        return -1
+
