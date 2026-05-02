52659c478abb20cb6364302d3d831b5afeef7367
Daniel Tilahun <106193338+Danitilahun@users.noreply.github.com>
2026-05-03 00:23:18 +0300

    Time: 23 ms (65.93%), Space: 19.3 MB (69.05%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -3,23 +3,31 @@ class Solution:
         count = 0
         valid_nums = {2,5,6,9}
         invalid_nums = {3,4,7}
-
+        memo = {}
         for num in range(2,n+1):
             valid = False
 
             if num in invalid_nums:
                 continue
             
+            orginal_num = num
+
             while num:
+                 
                 digit = num%10
                 if digit in valid_nums:
                     valid = True
+                    prefix= num // 10
+                    if prefix in memo:
+                        break
                 elif digit in invalid_nums:
                     valid = False
                     break
                 num //= 10
+
             
             if valid:
+                memo[orginal_num//10]= True
                 count += 1
         
         return count
