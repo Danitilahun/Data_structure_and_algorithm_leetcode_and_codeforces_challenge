c58d7006be79059dfa90a7d15b582e09cef7dfc0
Daniel Tilahun <106193338+Danitilahun@users.noreply.github.com>
2026-06-17 11:57:34 +0300

    Time: 137 ms (5.32%), Space: 23.3 MB (28.72%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
@@ -7,12 +7,12 @@ class Solution:
         flips_count = [0] * (n+1)
         current_flips_count = 0
         flip_total = 0
+        impossiblity_check = False
 
         for ind in range(n):
 
             
             current_flips_count+=flips_count[ind]
-            # print(current_flips_count,flips_count)
 
             flip = current_flips_count % 2
             
@@ -24,10 +24,9 @@ class Solution:
             
             if current_flips_count%2:
                 nums[ind] = 0 if nums[ind] == 1 else 1
+            
+            if nums[ind] == 0: impossiblity_check = True
 
-        # print(nums)
-        
-        impossiblity_check = any(nums[ind]==0 for ind in range(n))
 
         return flip_total if impossiblity_check == False else -1
 
